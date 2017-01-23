5e73affa89aa0a6e8f023e7c3f32f8c2e60c81bd
Shashank <shashank96.nayak@gmail.com>
2017-01-23 22:57:58 +0530

    old commit

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 
- var appID = process.env.MY_APP_ID || "MISSING ID";
- var appPassword = process.env.MY_APP_PASSWORD || "MISSING PASSWORD";
+ 
 
 //Creating restify server
 var server = restify.createServer();
 server.listen(process.env.PORT || 3978, function(){
     console.log('%s listening to %s',server.name, server.url);
 });
-server.get('/', restify.serveStatic({
+
+/*server.get('/', restify.serveStatic({
  directory: __dirname,
  default: '/index.html'
 }));
+*/
 
 //Create a bot
 var connector = new builder.ChatConnector({
-    appID: process.env.MY_APP_ID,
-    appPassword: process.env.MY_APP_PASSWORD
+    appID: '123',
+    appPassword: '123'
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
