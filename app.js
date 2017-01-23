f0f6a0ae1b0a7e56c9a1d3790d81be63a869fb2b
Shashank <shashank96.nayak@gmail.com>
2017-01-23 17:32:55 +0530

    Updated server.get code

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ var server = restify.createServer();
 server.listen(process.env.PORT || 3978, function(){
     console.log('%s listening to %s',server.name, server.url);
 });
+server.get('/', restify.serveStatic({
+ directory: __dirname,
+ default: 'index.html'
+}));
 
 //Create a bot
 var connector = new builder.ChatConnector({
