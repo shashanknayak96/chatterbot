23ea418ef0250c084c614de4f1cab0b09dc17997
Shashank <shashank96.nayak@gmail.com>
2017-01-23 16:47:17 +0530

    Variables

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ var builder = require('botbuilder');
 
 //Creating restify server
 var server = restify.createServer();
-server.listen(process.env.PORT || 3000, function(){
+server.listen(process.env.PORT || 3978, function(){
     console.log('%s listening to %s',server.name, server.url);
 });
 
 //Create a bot
 var connector = new builder.ChatConnector({
-    appID:'8812b9b3-1f99-47a3-bd31-5b65e4608823',
-    appPassword:'3bsGSXhfzbjbcuqCQnNhgXC'
+    appID:process.env.MY_APP_ID,
+    appPassword:process.env.MY_APP_PASSWORD
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
